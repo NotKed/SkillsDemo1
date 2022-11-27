6df26c3287c938535810388872fd87175e0ec8c5
NotKed <kyleedwards031003@gmail.com>
2022-11-27 20:09:12 +0000

    create a student and store data

--- src/com/notked/Main.java
+++ src/com/notked/Main.java
@@ -1,7 +1,5 @@
 package com.notked;
 
-import java.util.Scanner;
-
 /**
  * 
  * @author kyleedwards
@@ -11,55 +9,10 @@ import java.util.Scanner;
 
 public class Main {
 
+	static Student handler = new Student();
+	
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		// We pre-define our variables just to make the code neater.
-		String name, id, course;
-		boolean resident = false;
-		String[] modules, grades, credits;
-		
-		
-		System.out.println("Please enter the following details.\nStudent name: ");
-		// Use scanner to retrieve the name of the student and store it in a variable for later use.
-		name = scanner.next();
-		
-		System.out.println("Student ID: \n");
-		id = scanner.next();
-		
-		System.out.println("Course studied: \n");
-		course = scanner.next();
-		
-		// For the residency field, we will ask for a true or false statement from the user.
-		// Since resident is pre-defined as false, we only have to change it if the answer is true.
-		System.out.println("Does the student live on campus? (Please answer 'true' or 'false')\n");
-		String nextScan = scanner.next();
-		if(nextScan.equalsIgnoreCase("true")) resident = true;
-		
-		// For the next batch of data, we are looking at 3 different variables per field.
-		// We will use lists to store this data, and use formatting to read it from the user.
-		
-		System.out.println("Number of modules completed per semester.");
-		System.out.println("Please provide the data using this format. 'semester 1/semester 2/semester 3' (without '')");
-		System.out.println("Example: '5/4/7'\n");
-		modules = scanner.next().split("/");
-		
-		// Instead of spamming the console, we will just advise the user to use the same format for each variable.
-		System.out.println("Overall grade per semester.");
-		System.out.println("Please use the same format provided for the last input.\n");
-		grades = scanner.next().split("/");
-		
-		System.out.println("Credits achieved per semester.");
-		System.out.println("Please use the same format provided for the last input.\n");
-		credits = scanner.next().split("/");
-		
-		System.out.println(name);
-		System.out.println(id);
-		System.out.println(course);
-		System.out.println(resident);
-		System.out.println(modules);
-		System.out.println(grades);
-		System.out.println(credits);
+		handler.createStudent();
 	}
 	
 	
--- src/com/notked/Student.java
+++ src/com/notked/Student.java
@@ -8,12 +8,10 @@ public class Student {
 	private int id;
 	private String course;
 	private boolean resident;
-	private int[] modules;
-	private int[] grades;
-	private Main main;
+	private int[] modules = new int[3];
+	private int[] grades = new int[3];
 	
-	public Student(Main main) {
-		this.main = main;
+	public Student() {
 		// String name, int id, String course, boolean resident, int[] modules, int[] grades
 //		this.name = name;
 //		this.id = id;
@@ -24,9 +22,48 @@ public class Student {
 	}
 
 	public void createStudent() {
+		// We use a scanner to read data in from the console.
 		Scanner scanner = new Scanner(System.in);
-		print("What is the students name?: \n");
-		name = scanner.next();
+		
+		// We begin by asking the user details about the student to store.
+		print("Please enter the following details.\nStudent name?: \n");
+		name = scanner.nextLine();
+		
+		print("Student ID?: \n");
+		// Because the ID is in the form of an integer and we receive the lines in the form of Strings, we must parse it as an integer.
+		id = Integer.parseInt(scanner.nextLine());
+		
+		print("Student Course?: \n");
+		course = scanner.nextLine();
+		
+		// Much like the integer, we require a boolean for the residency question. We will use an if statement to determine this.
+		print("Does the student live on campus?: (Please answer 'true' or 'false')");
+		String nextScan = scanner.nextLine();
+		if(nextScan.equalsIgnoreCase("true")) resident = true;
+		else resident = false;
+		
+		// Moving onto the next fields, we are looking at 3 different variables per field. To make it easier, we will use formatting 
+		// to receive this data, then break it down into the integer lists that we want to store it in.
+		print("Number of modules completed per semester.");
+		print("Please provide the data using this format. 'semester 1/semester 2/semester 3' (without '')");
+		print("Example: '5/4/7'\n");
+		String[] moduleStrings = scanner.nextLine().split("/");
+		for(int i = 0; i < moduleStrings.length; i++) {
+			modules[i] = Integer.parseInt(moduleStrings[i]);
+		}
+		
+		// We can use the same bit of code for the next question, as our answers should be relatively similar, at least the formatting will be.
+		print("Overall grade per semester.");
+		print("Please provide the data using this format. 'semester 1/semester 2/semester 3' (without '')");
+		print("Example: '5/4/7'\n");
+		String[] gradeStrings = scanner.nextLine().split("/");
+		for(int i = 0; i < gradeStrings.length; i++) {
+			grades[i] = Integer.parseInt(gradeStrings[i]);
+		}
+		
+		for(int i = 0; i <grades.length; i++) {
+			System.out.println(grades[i]);
+		}
 	}
 	
 	public void print(String s) {
